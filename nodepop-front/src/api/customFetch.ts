f85a9e634c65420b58a9cf20af328c9e40408a39
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-11-15 18:39:39 +0100

    feat: add interceptor for accessToken

--- nodepop-front/src/api/customFetch.ts
+++ nodepop-front/src/api/customFetch.ts
@@ -3,4 +3,19 @@ const url = import.meta.env.VITE_BASE_URL;
 
 const customFetch = axios.create({ baseURL: url });
 customFetch.interceptors.response.use(({ data }) => data);
+customFetch.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem('accessToken');
+
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
 export default customFetch;
