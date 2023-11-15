4ff003acfcf3579aef20498dd0cbf9eb5de477f9
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-11-15 18:39:56 +0100

    feat: add Spinner component to components index

--- nodepop-front/src/components/index.ts
+++ nodepop-front/src/components/index.ts
@@ -1,2 +1,4 @@
 export { default as Logo } from './Logo';
 export { default as SubmitButton } from './SubmitButton';
+export { default as FormRow } from './form/FormRow';
+export { default as Spinner } from './Spinner';
