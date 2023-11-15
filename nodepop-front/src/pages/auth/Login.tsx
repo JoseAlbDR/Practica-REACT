78da593b2c9dd6573f5d2bc1e1d1f0245ca70def
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-11-15 18:00:43 +0100

    feat: Login page

---
+++ nodepop-front/src/pages/auth/Login.tsx
@@ -0,0 +1,72 @@
+import {
+  ActionFunctionArgs,
+  Form,
+  redirect,
+  useNavigation,
+} from 'react-router-dom';
+import StyledSignup from './AuthWrapper';
+import Logo from '../../components/Logo';
+import FormRow from '../../components/form/FormRow';
+import SubmitButton from '../../components/SubmitButton';
+import { Link } from 'react-router-dom';
+import { login } from './service';
+import { AxiosError } from 'axios';
+import { toast } from 'react-toastify';
+
+export const action = async (data: ActionFunctionArgs) => {
+  const { request } = data;
+  const formData = await request.formData();
+
+  const email = formData.get('email') as string;
+  const password = formData.get('password') as string;
+
+  try {
+    await login({ email, password });
+    toast.success('User Succesfully Logged In');
+    return redirect('/login');
+  } catch (error) {
+    if (error instanceof AxiosError) {
+      console.log(error);
+      toast.error('Wrong Username or Password');
+    }
+    console.log({ error });
+    return error;
+  }
+};
+
+const Signup = () => {
+  const navigation = useNavigation();
+  const isSubmitting = navigation.state === 'submitting';
+
+  return (
+    <StyledSignup>
+      <Form method="post" className="form">
+        <Logo />
+        <h4>Sign Up</h4>
+        <FormRow
+          type="email"
+          name="email"
+          labelText="email"
+          defaultValue="yusepah@gmail.com"
+          disabled={isSubmitting}
+        ></FormRow>
+        <FormRow
+          type="password"
+          name="password"
+          labelText="password"
+          defaultValue="mekieros"
+          disabled={isSubmitting}
+        ></FormRow>
+        <SubmitButton />
+        <p>
+          Not a Member Yet?
+          <Link to="/login" className="member-btn">
+            Signup
+          </Link>
+        </p>
+      </Form>
+    </StyledSignup>
+  );
+};
+
+export default Signup;
