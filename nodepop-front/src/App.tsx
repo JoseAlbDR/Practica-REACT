033aff9a935e7fbcd6ee782f434a1b5c6dc72476
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-11-15 18:38:53 +0100

    feat: add Adverts page to router

--- nodepop-front/src/App.tsx
+++ nodepop-front/src/App.tsx
@@ -4,8 +4,15 @@ import ErrorPage from './pages/error/ErrorPage';
 import Signup from './pages/auth/Signup';
 import Login from './pages/auth/Login';
 
+// Actions
 import { action as signupAction } from './pages/auth/Signup';
 import { action as loginAction } from './pages/auth/Login';
+
+// Loaders
+import { loader as currentUserLoader } from './pages/adverts/Adverts';
+
+import Landing from './pages/landing/Landing';
+import Adverts from './pages/adverts/Adverts';
 
 const router = createBrowserRouter([
   {
@@ -15,14 +22,22 @@ const router = createBrowserRouter([
     children: [
       {
         index: true,
+        element: <Landing />,
+      },
+      {
+        path: '/signup',
         element: <Signup />,
-        errorElement: <ErrorPage />,
         action: signupAction,
       },
       {
         path: '/login',
         element: <Login />,
         action: loginAction,
+      },
+      {
+        path: '/adverts',
+        element: <Adverts />,
+        loader: currentUserLoader,
       },
     ],
   },
