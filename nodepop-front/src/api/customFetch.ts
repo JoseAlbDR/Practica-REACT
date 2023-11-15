036952e5ea6fa77cb3e783a27cab7f379132beae
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-11-15 16:48:52 +0100

    feat: customFetch Api

---
+++ nodepop-front/src/api/customFetch.ts
@@ -0,0 +1,6 @@
+import axios from 'axios';
+const url = import.meta.env.VITE_BASE_URL;
+
+const customFetch = axios.create({ baseURL: url });
+customFetch.interceptors.response.use(({ data }) => data);
+export default customFetch;
