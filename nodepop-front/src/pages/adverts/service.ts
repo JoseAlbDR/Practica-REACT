97afeb326f239a9efefc0ccc7daf459131479240
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-11-16 11:10:12 +0100

    feat: getAllAdverts service

--- nodepop-front/src/pages/adverts/service.ts
+++ nodepop-front/src/pages/adverts/service.ts
@@ -1,7 +1,14 @@
+// import { AxiosPromise } from 'axios';
 import customFetch from '../../api/customFetch';
+// import { Advert } from '../../interfaces/advert.interface';
 
 export const getUser = async () => {
   const user = await customFetch.get('/auth/me');
   console.log(user);
   return user;
 };
+
+// export const getAllAdverts = async (): AxiosPromise<Advert[]> => {
+//   const adverts = await customFetch.get('/v1/adverts');
+//   return adverts;
+// };
