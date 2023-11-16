4dd74b6f78bcff0b2697073717d54599710e4515
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-11-16 11:08:46 +0100

    feat: complete NavBar component

--- nodepop-front/src/components/navBar/NavBar.tsx
+++ nodepop-front/src/components/navBar/NavBar.tsx
@@ -1,10 +1,19 @@
-import { useLoaderData } from 'react-router-dom';
+import { useLoaderData, useNavigate } from 'react-router-dom';
 import Wrapper from './styles/NavBarWrapper';
 import { Logo } from '..';
 import { Link } from 'react-router-dom';
+import { toast } from 'react-toastify';
+import { IUser } from '../../interfaces/auth.interfaces';
 
 const NavBar = () => {
-  const user = useLoaderData();
+  const user = useLoaderData() as IUser;
+  const navigate = useNavigate();
+
+  const logout = () => {
+    localStorage.removeItem('accessToken');
+    toast.success(`User ${user.name} successfully logged out`);
+    navigate('/login');
+  };
 
   return (
     <Wrapper>
@@ -25,7 +34,9 @@ const NavBar = () => {
               <Link to="/" className="btn menu-btn">
                 New Advert
               </Link>
-              <button className="btn btn-hipster menu-btn">Logout</button>
+              <button onClick={logout} className="btn btn-hipster menu-btn">
+                Logout
+              </button>
             </>
           )}
         </ul>
