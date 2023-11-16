a29d0a37b8e5f263cc2912a3d6722ab59d2a420e
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-11-16 11:04:38 +0100

    refactor: move all interfaces to interfaces folder

---
+++ nodepop-front/src/interfaces/tags.interface.ts
@@ -0,0 +1,1 @@
+export type ITags = 'motor' | 'work' | 'mobile' | 'lifestyle';
---
+++ nodepop-front/src/interfaces/user.interface.ts
@@ -0,0 +1,7 @@
+export interface IUser {
+  createdAt: string;
+  email: string;
+  id: string;
+  name: string;
+  username: string;
+}
