884e3c08eac6d3a6614b86159145e562651babde
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-11-16 11:05:27 +0100

    feat: create component FormRowSelect

---
+++ nodepop-front/src/components/form/FormRowSelect.tsx
@@ -0,0 +1,40 @@
+import { SubmitFunction } from 'react-router-dom';
+
+interface IFormSelectProps {
+  name: string;
+  types: string[];
+  selected?: string;
+  onChange?: SubmitFunction;
+}
+const FormRowSelect = ({
+  name,
+  types,
+  selected = '',
+}: // onChange = () => {},
+IFormSelectProps) => {
+  return (
+    <div className="form-row">
+      <label className="form-label" htmlFor={name}>
+        {name === 'onSale' ? 'type' : name}
+      </label>
+      <select
+        name={name === 'category' ? 'tags' : name}
+        id={name}
+        className="form-input"
+        defaultValue={selected}
+        // onChange={(e) => {
+        //   if (e.currentTarget.form) changePriceUrl(e.currentTarget.form);
+        //   return onChange(e.currentTarget.form);
+        // }}
+      >
+        {types.map((type) => (
+          <option key={type} value={type}>
+            {type}
+          </option>
+        ))}
+      </select>
+    </div>
+  );
+};
+
+export default FormRowSelect;
