b773a19748a457ccdea7cedf2b3c74cc9d6fd077
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-11-16 11:05:50 +0100

    feat: create component FormRowTags

---
+++ nodepop-front/src/components/form/FormRowTags.tsx
@@ -0,0 +1,33 @@
+import { useLoaderData } from 'react-router-dom';
+import { ITags } from '../../interfaces/tags.interface';
+
+interface FormRowTagsProps {
+  tags?: ITags[] | undefined;
+}
+
+const FormRowTags = ({ tags = undefined }: FormRowTagsProps) => {
+  const renderTags = useLoaderData() as ITags[];
+  console.log(renderTags);
+
+  return (
+    <div className="form-row">
+      <label className="form-label">tags</label>
+      <fieldset className="form-tags">
+        {renderTags.map((tag: ITags) => (
+          <label key={tag}>
+            <input
+              type="checkbox"
+              name="tags"
+              value={tag}
+              className="input-check"
+              defaultChecked={tags && tags.includes(tag)}
+            />
+            {tag}
+          </label>
+        ))}
+      </fieldset>
+    </div>
+  );
+};
+
+export default FormRowTags;
