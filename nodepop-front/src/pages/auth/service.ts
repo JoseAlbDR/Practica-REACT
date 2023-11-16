4dfcbd2fa16929844e058a80a47f054a93d8e1b1
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-11-16 18:10:14 +0100

    feat: setAuthorizationToken in login and store accessToken in localStorage if remember me checkbox is checked

--- nodepop-front/src/pages/auth/service.ts
+++ nodepop-front/src/pages/auth/service.ts
@@ -1,16 +1,17 @@
-import customFetch from '../../api/customFetch';
+import customFetch, { setAuthorizationHeader } from '../../api/customFetch';
 import { ILogin, IUser } from '../../interfaces/auth.interfaces';
 
 export const signup = async (user: IUser) => {
-  const response = await customFetch.post('/auth/signup', user);
-  console.log(response);
-  return response;
+  const userData = await customFetch.post('/auth/signup', user);
+  return userData;
 };
 
 export const login = async (user: ILogin, remember: boolean) => {
   const { accessToken } = (await customFetch.post('auth/login', user)) as {
     accessToken: string;
   };
+
+  setAuthorizationHeader(accessToken);
 
   if (remember) localStorage.setItem('accessToken', accessToken);
 };
