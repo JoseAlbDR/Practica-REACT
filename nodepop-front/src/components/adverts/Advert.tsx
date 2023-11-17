f20328353aaa9c680cb77d2b1ae018b828018239
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-11-17 12:51:35 +0100

    feat: load no image available png it here is no image for advert

--- nodepop-front/src/components/adverts/Advert.tsx
+++ nodepop-front/src/components/adverts/Advert.tsx
@@ -3,14 +3,25 @@ import { FaMoneyBill } from 'react-icons/fa';
 import Wrapper from './styles/AdvertWrapper';
 import onSale from '../../assets/images/sell.svg';
 import search from '../../assets/images/search.svg';
-
+import errorImg from '../../assets/images/Image_not_available.png';
 import { AdvertInfo, AdvertTags } from '../';
 import { IAdvert } from '../../interfaces/advert.interface';
+import { SyntheticEvent } from 'react';
 
 const Advert = ({ name, sale, price, tags, photo }: IAdvert) => {
+  const handleImageError = (event: SyntheticEvent<HTMLImageElement>) => {
+    console.log('error');
+    event.currentTarget.src = '../../assets/images/Image_not_available.png';
+  };
+
   return (
     <Wrapper>
-      <img src={photo} alt={`${name} image`} className="img" />
+      <img
+        src={photo || errorImg}
+        alt={`${name} image`}
+        onError={handleImageError}
+        className="img"
+      />
       <div className="content">
         <div className="content-header">
           <div className={`status ${sale ? 'on-sale' : 'search'}`}>
