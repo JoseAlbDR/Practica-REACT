9a53c3d96d646e07ff05dcb01292bd4e57a9cbe5
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-11-18 11:25:19 +0100

    feat: handle Axios not found error

--- nodepop-front/src/utils/getError.ts
+++ nodepop-front/src/utils/getError.ts
@@ -1,11 +1,18 @@
 import { isRouteErrorResponse } from 'react-router-dom';
 import { IError } from '../interfaces/error.interfaces';
+import { AxiosError } from 'axios';
 
 export const getError = (error: unknown): IError => {
+  console.log(error instanceof AxiosError);
   if (isRouteErrorResponse(error)) {
     return {
       msg: error.statusText,
       status: error.status,
+    };
+  } else if (error instanceof AxiosError) {
+    return {
+      msg: 'Advert not found!',
+      status: error.response?.status,
     };
   } else if (error instanceof Error) {
     return { msg: error.message };
