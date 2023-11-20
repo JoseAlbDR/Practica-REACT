3472dbe4cd86a11548892d8fa7e3407e981e2a50
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-11-20 16:43:18 +0100

    feat: add new utils to utils index

--- nodepop-front/src/utils/index.ts
+++ nodepop-front/src/utils/index.ts
@@ -1,3 +1,6 @@
+export { changePriceUrl } from './changePriceUrl';
+export { checkToken } from './checkToken';
+export { getMinMaxPrice } from './getMinMaxPrice';
 export { getError } from './getError';
 export { storage } from './storage';
 export { checkRememberMe } from './checkRememberMe';
