1e59ba92629f2000ccd43933b851ed63195b0bb3
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-11-21 16:31:11 +0100

    feat: Error message when there are no tags selected

--- nodepop-front/src/components/form/FormRowTags.tsx
+++ nodepop-front/src/components/form/FormRowTags.tsx
@@ -1,5 +1,6 @@
 import { ITags } from '../../interfaces/tags.interface';
 import { useTags } from '../../context/TagsContext';
+import ErrorComponent from '../shared/ErrorComponent';
 
 interface FormRowTagsProps {
   tags?: ITags[] | undefined;
@@ -11,6 +12,9 @@ const FormRowTags = ({ tags = undefined }: FormRowTagsProps) => {
   return (
     <div className="form-row">
       <label className="form-label">tags</label>
+      {tags?.length === 0 && (
+        <ErrorComponent message="Select At Least One Tag" />
+      )}
       <fieldset className="form-tags">
         {renderTags?.map((tag: ITags) => (
           <label key={tag}>
