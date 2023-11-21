bcee3f81265142ee353839f6c2ccf77d43f7f9bc
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-11-21 09:55:15 +0100

    feat: add interceptor for Axios error in api

--- nodepop-front/src/api/customFetch.ts
+++ nodepop-front/src/api/customFetch.ts
@@ -1,8 +1,23 @@
 import axios from 'axios';
+
 const url = import.meta.env.VITE_BASE_URL;
 
 const customFetch = axios.create({ baseURL: url });
-customFetch.interceptors.response.use(({ data }) => data);
+customFetch.interceptors.response.use(
+  (response) => response.data,
+  (error) => {
+    if (error.response) {
+      console.log(error.response);
+      return Promise.reject({
+        message: error.response.statusText,
+        ...error.response,
+        ...error.response.data,
+      });
+    }
+    console.log(error);
+    return Promise.reject({ message: error.message });
+  }
+);
 
 export const setAuthorizationHeader = (token: string) => {
   customFetch.defaults.headers.common['Authorization'] = `Bearer ${token}`;
