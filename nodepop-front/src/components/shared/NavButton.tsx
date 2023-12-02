07dfe0ae7665163da948408653bbdc0fb7f34356
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-12-02 16:06:08 +0100

    refactor: NavButtonProps

--- nodepop-front/src/components/shared/NavButton.tsx
+++ nodepop-front/src/components/shared/NavButton.tsx
@@ -1,12 +1,17 @@
 import { NavLink } from 'react-router-dom';
 import styled from 'styled-components';
 
+interface NavButtonProps {
+  to: string;
+  name: string;
+}
+
 const StyledNavLink = styled(NavLink)`
   background: var(--primary-100);
   color: var(--green-dark);
 `;
 
-const NavButton = ({ to, name }: { to: string; name: string }) => {
+const NavButton = ({ to, name }: NavButtonProps) => {
   return (
     <StyledNavLink to={to} className="btn menu-btn" end>
       {name}
