6f738854239428099d1c6ed872c067d09028491b
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2024-01-25 19:29:27 +0100

    refactor: configureStore with rtk

--- nodepop-front/src/store/index.ts
+++ nodepop-front/src/store/index.ts
@@ -1,36 +1,32 @@
-import { createStore, combineReducers, applyMiddleware, Action } from 'redux';
+import { configureStore as rtkConfigureStore } from '@reduxjs/toolkit';
 
 import * as reducers from './reducers';
 import * as auth from '../pages/auth/service';
 import * as adverts from '../pages/adverts/service';
 import * as actionCreators from './actions';
 
-import { withExtraArgument } from 'redux-thunk';
-import { composeWithDevTools } from '@redux-devtools/extension';
-import { Auth, ReduxState } from '../interfaces/state.interface';
-import type { Router } from '@remix-run/router';
-import { Api } from './actions/action.interfaces';
-
-const composeEnhancers = composeWithDevTools({ actionCreators });
+import { Auth } from '../interfaces/state.interface';
+import { Router } from 'react-router-dom';
 
 export default function configureStore(
   preloadedState: { auth: Auth },
   { router }: { router: Router }
 ) {
-  const middlewares = withExtraArgument<
-    ReduxState,
-    Action,
-    { api: Api; router: Router }
-  >({
-    api: { auth, adverts },
-    router,
-  });
-
-  const store = createStore(
-    combineReducers(reducers),
+  const store = rtkConfigureStore({
+    reducer: reducers,
     preloadedState,
-    composeEnhancers(applyMiddleware(middlewares))
-  );
+    devTools: { actionCreators },
+    middleware: (getDefaultMiddleware) =>
+      getDefaultMiddleware({
+        thunk: {
+          extraArgument: {
+            api: { auth, adverts },
+            router,
+          },
+        },
+        serializableCheck: false,
+      }),
+  });
 
   return store;
 }
